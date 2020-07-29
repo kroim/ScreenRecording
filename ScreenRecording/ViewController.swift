29d015f8668a3d3892bea5c5f086e7ff22581deb
kgcdream2019 <kgc.dream2019@gmail.com>
2020-07-29 10:14:56 +0800

    Initial Commit

---
+++ ScreenRecording/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenRecording
+//
+//  Created by Zhang Xiqian on 2020/7/29.
+//  Copyright © 2020 RUBY MAE Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
